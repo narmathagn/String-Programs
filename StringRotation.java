4e86cc919e3ee7019a7ddea1c615a6ede909539f
narmathagn <42265938+narmathagn@users.noreply.github.com>
2020-07-25 00:37:27 -0700

    Add files via upload

---
+++ StringRotation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class StringRotation{
+	public static void main(String args[]){
+		String s="rhdt:246";
+		String[] s1=s.split(":");
+		String ans="";
+		char[] ch=s1[1].toCharArray();
+		int sum=0;
+		for(int i=0;i<ch.length;i++)
+		{
+			sum=sum+(ch[i]*ch[i]);
+		}
+		if(sum%2==0)
+		{
+			ans=s1[0].substring(s1[0].length()-1)+s1[0].substring(0,s1[0].length()-1);
+		}
+		else
+		{
+			ans=s1[0].substring(2)+s1[0].substring(0,2);
+		}
+		System.out.println(ans);
+	}
+}
